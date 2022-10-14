bd650bc2dcb128e2610b60695604c10d827e15cd
Ben Kurose <ben.kurose@gmail.com>
2022-10-14 13:13:58 -0400

    commit tree c1f41a63a97e3c73ec204eb10fa0b301e0a35d4d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_entry_message
+    
+  private
+
+  def invalid_entry_message(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
